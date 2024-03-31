8caf587def2cfe354367f9398eb99d9fa4a17af2
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-04-01 00:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function shortcut(s1, s2) {
   // your code
 	let ans = '';
-	if (!s1 || !s2) {
+	if (s1.length==0 || s2.length==0) {
 		return ans;
 	}
 	else {
-		ans+ = s1[0] + s2[0];;
+		ans+= s1[0] + s2[0];;
 		return ans;
 	}
 }
