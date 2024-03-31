9d7b184d695fd826ce4694b7aa4948af9c4fad4a
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-31 23:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function shortcut(s1, s2) {
   // your code
+	let ans = '';
 	if (s1.length==0 || s2.length==0) {
-		return ''
+		return ans;
 	}
 	else {
-		console.log(s1[0] + s2[0])
+		ans+ = s1[0];
+		ans+ = s2[0];
+		return ans;
 	}
 	
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
